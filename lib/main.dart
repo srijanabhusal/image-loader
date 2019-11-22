6ddf4fb556ba12bacbbc884f59d7ce68e5904971
srijanabhusal <072bex440@ioe.edu.np>
2019-11-22 07:15:46 +0545

    image added from network

--- lib/main.dart
+++ lib/main.dart
@@ -14,11 +14,32 @@ import 'package:flutter/material.dart';
          appBar: AppBar(
            title: Text('image loader'),
          ),
-         body: Container(
-           child: Center(child:             
-             Image.asset("img/lamp.JPG")
-           ),
-         ),
+         body: Column(children: <Widget>[
+                Text("This image is displayed from local storage"),
+
+                Row(children: <Widget>[
+                    Container(
+                    padding: EdgeInsets.only(left: 100),
+                    child: Image.asset("img/lamp.JPG"),
+                    height: 200.0,
+                    width: 200.0,
+                    ),         
+                  ],),
+
+                Text("This image is displayed directly from internet"),
+
+                Row(children: <Widget>[
+                  Container(
+                  padding: EdgeInsets.only(left: 100), 
+                  child:Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRr7rkFzEQdJxTsMThnNZEvJnKUIfpsxp7P5od6LWNONTb40UzB_Q&s"),
+                  height: 200.0,
+                  width: 200.0,
+                ),
+                ],)
+
+
+               ],)
        );
      }
    }
+
